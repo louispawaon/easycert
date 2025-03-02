e528639c88ec4f401c782c32b88a07463bc1cc15
louispawaon <lpawaon@gmail.com>
2025-03-02 19:34:30 +0800

    refactor: opengraph attempt 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,28 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'EasyCert - Certificate Generation Made Easy',
   description: 'Automate your certificate generation process with EasyCert',
-  openGraph:{
+  openGraph: {
     title: 'EasyCert - Certificate Generation Made Easy',
     description: 'Automate your certificate generation process with EasyCert',
-    url: "https://easycert.vercel.app/"
-  }
+    url: 'https://easycert.vercel.app/',
+    siteName: 'EasyCert',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'EasyCert - Certificate Generation Made Easy',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'EasyCert - Certificate Generation Made Easy',
+    description: 'Automate your certificate generation process with EasyCert',
+    images: ['/opengraph-image.png'],
+  },
 };
 
 export default function RootLayout({
