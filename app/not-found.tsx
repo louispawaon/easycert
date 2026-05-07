de38493e72ab6e31e2f849552a93b3c488e24543
louispawaon <lpawaon@gmail.com>
2026-05-07 11:56:11 +0800

    feat: updated 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-background text-foreground">
+      <h1 className="text-center text-xl font-medium uppercase sm:text-2xl">
+        YOU FOUND A BLANK SPACE IN THE INTERNET.
+      </h1>
+    </main>
+  );
+}
